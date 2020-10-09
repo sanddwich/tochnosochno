3c154ad9be401f49dac56cf92554a38a08d4674a
Denis <bck-dkiselev@yandex.ru>
2020-10-09 12:14:39 +0400

    1232313

--- front/src/Components/MainLayout/Header/HeaderUp/HeaderUp.tsx
+++ front/src/Components/MainLayout/Header/HeaderUp/HeaderUp.tsx
@@ -1,4 +1,6 @@
 import React from 'react'
+import { Container } from 'react-bootstrap'
+
 import './HeaderUp.scss'
 
 interface HeaderUpProps {}
@@ -8,7 +10,11 @@ interface HeaderUpState {}
 export default class HeaderUp extends React.Component<HeaderUpProps, HeaderUpState> {
   render() {
     return (
-      <h1>HeaderUp</h1>
+      <Container fluid className="HeaderUp p-0 m-0 d-flex justify-content-between">
+        <div className="HeaderUp__menuItem">
+          Акции
+        </div>
+      </Container>
     )
   }
 }
