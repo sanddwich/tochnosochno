7eb3afd717420ad457dac1fde8403da494df90c0
denristun <58968932+denristun@users.noreply.github.com>
2020-10-09 12:03:58 +0400

    11
    11

--- front/src/Components/MainLayout/Footer/Footer.tsx
+++ front/src/Components/MainLayout/Footer/Footer.tsx
@@ -1,4 +1,5 @@
 import React from 'react'
+import RoundButton from '../../../SharedComponents/RoundButton/RoundButton'
 import './Footer.scss'
 
 interface FooterProps {}
@@ -8,7 +9,32 @@ interface FooterState {}
 export default class Footer extends React.Component<FooterProps, FooterState> {
   render() {
     return (
-      <h1>Footer</h1>
+      <footer className="footer">
+        <div className="container">
+          <div className="row">
+            <div className="col-md-6 d-flex">
+              <div className="row">
+                <img src="images/logo.svg" alt="Logo" />
+                <div className="footer__social">
+                  Социальные сети
+                  <RoundButton
+                    icon="instagram.svg"
+                    backgroundColor="white"
+                    onClick={() => window.open('http://instagram.com')}
+                  />
+                  <RoundButton icon="vk.svg" backgroundColor="white" onClick={() => window.open('http://vk.com')} />
+                </div>
+              </div>
+
+              <div className="row"></div>
+            </div>
+
+            <div className="col-md-3"></div>
+
+            <div className="col-md-3"></div>
+          </div>
+        </div>
+      </footer>
     )
   }
 }
--- front/src/SharedComponents/RoundButton/RoundButton.tsx
+++ front/src/SharedComponents/RoundButton/RoundButton.tsx
@@ -3,16 +3,24 @@ import { Container } from 'react-bootstrap'
 
 import './RoundButton.scss'
 
-interface RoundButtonProps {}
+interface RoundButtonProps {
+  icon: string
+  onClick: any
+  backgroundColor: string
+}
 
 interface RoundButtonState {}
 
 export default class RoundButton extends React.Component<RoundButtonProps, RoundButtonState> {
   render() {
     return (
-      <Container className="RoundButton p-0 m-0">
-        <h1>RoundButton</h1>
-      </Container>
+      <div
+        onClick={this.props.onClick}
+        className="roundButton noselect"
+        style={{ backgroundColor: this.props.backgroundColor }}
+      >
+        <img src={`images/icons/${this.props.icon}`} alt="icon" />
+      </div>
     )
   }
 }
