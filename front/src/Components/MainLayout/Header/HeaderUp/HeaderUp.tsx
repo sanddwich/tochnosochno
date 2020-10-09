4578800f78f3c297afa68a1c2d70ff967e0e61ac
Denis <bck-dkiselev@yandex.ru>
2020-10-09 12:53:31 +0400

    213213

--- front/src/Components/MainLayout/Header/HeaderUp/HeaderUp.tsx
+++ front/src/Components/MainLayout/Header/HeaderUp/HeaderUp.tsx
@@ -1,5 +1,6 @@
 import React from 'react'
-import { Container } from 'react-bootstrap'
+import { Col, Container, Row } from 'react-bootstrap'
+import RoundButton from '../../../../SharedComponents/RoundButton/RoundButton'
 
 import './HeaderUp.scss'
 
@@ -8,11 +9,56 @@ interface HeaderUpProps {}
 interface HeaderUpState {}
 
 export default class HeaderUp extends React.Component<HeaderUpProps, HeaderUpState> {
+  instaButtonClick = (): void => {
+    console.log('instaButtonClick')
+  }
+
+  vkButtonClick = (): void => {
+    console.log('vkButtonClick')
+  }
+
   render() {
     return (
-      <Container fluid className="HeaderUp p-0 m-0 d-flex justify-content-between">
+      <Container className="HeaderUp p-0 d-flex justify-content-between">
+        <div className="HeaderUp__menuItem">Акции</div>
+        <div className="HeaderUp__menuItem">Соц.сети:</div>
         <div className="HeaderUp__menuItem">
-          Акции
+          <Row className="m-0 p-0">
+            <Col className="m-0 p-0">
+              <RoundButton backgroundColor="#303030" icon="insta_white.svg" onClick={() => this.instaButtonClick()} />
+            </Col>
+            <Col className="">
+              <RoundButton backgroundColor="#303030" icon="vk_white.svg" onClick={() => this.vkButtonClick()} />
+            </Col>
+          </Row>
+        </div>
+        <div className="HeaderUp__menuItem">Контакты</div>
+        <div className="HeaderUp__menuItem">
+          <div className="HeaderUp__menuLogo">
+            <img src="images/logo_white.svg" alt="" />
+          </div>
+        </div>
+        <div className="HeaderUp__menuItem d-flex justify-content-between">
+          <Row className="HeaderUp__container p-0 m-0 d-flex align-items-center">
+            <div className="HeaderUp__containerImg">
+              <RoundButton backgroundColor="#303030" icon="nyamnyan_whyte.svg" onClick={() => this.instaButtonClick()} />
+            </div>
+            <div className="HeaderUp__containerDescr pl-2">
+              <div className="HeaderUp__containerDescrPhone">46-46-07</div>
+              <div className="HeaderUp__containerDescrTitle">номер ресторана</div>
+            </div>
+          </Row>
+        </div>
+        <div className="HeaderUp__menuItem">Соц.сети:</div>
+        <div className="HeaderUp__menuItem">
+          <Row className="m-0 p-0">
+            <Col className="m-0 p-0">
+              <RoundButton backgroundColor="#303030" icon="cart_white.svg" onClick={() => this.instaButtonClick()} />
+            </Col>
+            <Col className="">
+              <RoundButton backgroundColor="#303030" icon="user_white.svg" onClick={() => this.vkButtonClick()} />
+            </Col>
+          </Row>
         </div>
       </Container>
     )
