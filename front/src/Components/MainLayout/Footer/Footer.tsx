b67315f342507df28f114d9909b35048d6038723
denristun <58968932+denristun@users.noreply.github.com>
2020-10-12 16:18:00 +0400

    11
    11

--- front/src/Components/MainLayout/Footer/Footer.tsx
+++ front/src/Components/MainLayout/Footer/Footer.tsx
@@ -10,40 +10,77 @@ export default class Footer extends React.Component<FooterProps, FooterState> {
   render() {
     return (
       <footer className="footer">
-        <div className="container p-5">
+        <div className="container ">
           <div className="row">
-            <div className="col-md-6 d-flex">
-              <div className="row">
-                <img src="images/logo.svg" alt="Logo" />
-                <div className="footer__social">
-                  Социальные сети
-                  <RoundButton
-                    icon="instagram.svg"
-                    backgroundColor="white"
-                    onClick={() => window.open('http://instagram.com')}
-                  />
-                  <RoundButton icon="vk.svg" backgroundColor="white" onClick={() => window.open('http://vk.com')} />
+            <div className="col-md-7 d-flex">
+              <div className="row ">
+                <div className="col-5">
+                  <img src="images/logo.svg" alt="Logo" />
                 </div>
-                <div className="footer__contacts">
-                  <div className="row">
-                    <div className="col-6 m-0 p-0">
-                      <div className="footer__contats__title">Телефон ресторана</div>
-                      <div className="footer__contats__phone"> 46-46-07</div>
-                    </div>
-                    <div className="col-6 m-0 p-0">
-                      <div className="footer__contats__title">Телефон доставки</div>
-                      <div className="footer__contats__phone"> 46-46-02</div>
+                <div className="col-1"></div>
+
+                <div className="col-6 footer__social">
+                  <div className="row ">
+                    <div className="col-md-9 align-middle">Социальные сети</div>
+                    <div className="col-xl-3 d-flex">
+                      <div className="ml-1">
+                        <RoundButton
+                          icon="instagram.svg"
+                          backgroundColor="white"
+                          onClick={() => window.open('http://instagram.com')}
+                        />
+                      </div>
+                      <div className="ml-3">
+                        <RoundButton
+                          icon="vk.svg"
+                          backgroundColor="white"
+                          onClick={() => window.open('http://vk.com')}
+                        />
+                      </div>
                     </div>
                   </div>
                 </div>
               </div>
-
-              <div className="row"></div>
             </div>
 
-            <div className="col-md-3"></div>
+            <div className="col-md-5 ">
+              <div className="footer__worktime">Часы работы ресторана с 10:00 - 00:00</div>
+            </div>
+          </div>
+          <div className="row">
+            <div className="col-md-5">
+              <div className="footer__contacts">
+                <div className="row mt-3">
+                  <div className="col-6 pr-0">
+                    <div className="footer__contacts__title">Телефон ресторана</div>
+                    <div className="footer__contacts__phone"> 46-46-07</div>
+                  </div>
+                  <div className="col-6 pr-0">
+                    <div className="footer__contacts__title">Телефон доставки</div>
+                    <div className="footer__contacts__phone"> 46-46-02</div>
+                  </div>
+                </div>
+              </div>
+            </div>
+            <div className="col-md-4">
+              <div className="footer__policy">
+                <a href="">
+                  <div>Политика конфиденциальности</div>
+                </a>
+                <a href="">
+                  <div>Правила продажи товаров на сайте</div>
+                </a>
+              </div>
+            </div>
 
-            <div className="col-md-3"></div>
+            <div className="col-md-3 pr-0">
+              <div className="footer__copyright">
+                Сайт разработан
+                <a href="https://deedesign.ru">
+                  <span className="bold"> deedesign</span>
+                </a>
+              </div>
+            </div>
           </div>
         </div>
       </footer>
--- front/src/Components/MainLayout/MainLayout.tsx
+++ front/src/Components/MainLayout/MainLayout.tsx
@@ -4,12 +4,12 @@ import Footer from './Footer/Footer'
 import Header from './Header/Header'
 import './MainLayout.scss'
 import PageContent from './PageContent/PageContent'
-import { Route, Switch, Redirect } from 'react-router-dom';
+import { Route, Switch, Redirect } from 'react-router-dom'
 import Menu from '../Pages/Menu/Menu'
 import Main from '../Pages/Main/Main'
 import { connect } from 'react-redux'
 import { getMenu } from '../../Redux/actions/menu'
- 
+
 interface MainLayoutProps {
   getMenu: any
 }
@@ -25,23 +25,22 @@ class MainLayout extends React.Component<MainLayoutProps, MainLayoutState> {
     return (
       <Container fluid className="MainLayout p-0 m-0">
         <Header />
-        
+
         <PageContent>
           <Switch>
-            <Route path='/menu' exact component={Menu} />
-            <Route path='/' exact component={Main} />
+            <Route path="/menu" exact component={Menu} />
+            <Route path="/" exact component={Main} />
           </Switch>
         </PageContent>
-        
+
         <Footer />
       </Container>
     )
   }
 }
 
-
 const mapDispatchToProps = {
-  getMenu 
+  getMenu,
 }
 
 export default connect(null, mapDispatchToProps)(MainLayout)
