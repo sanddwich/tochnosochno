152a172f6eb12cee0669fadd33861942ece61ec0
denristun <58968932+denristun@users.noreply.github.com>
2020-10-21 16:12:54 +0400

    11
    11

--- front/src/SharedComponents/BlockName/BlockName.tsx
+++ front/src/SharedComponents/BlockName/BlockName.tsx
@@ -4,6 +4,7 @@ import './BlockName.scss'
 
 interface BlockNameProps {
   name: string
+  fontSize?: string
 }
 
 interface BlockNameState {}
@@ -14,7 +15,7 @@ export default class BlockName extends React.Component<BlockNameProps, BlockName
       <div className="BlockName">
         <div className="BlockName__cont">
           <div className="BlockName__title">
-            <h1>{this.props.name}</h1>
+            <h1 style={{ fontSize: this.props.fontSize }}>{this.props.name}</h1>
           </div>
           <div className="BlockName__underLineCont">
             <div className="BlockName__underLine"></div>
